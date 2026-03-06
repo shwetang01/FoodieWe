ce43698d46d93fc77baf7497940afd33d0e7be7a
Shwetang Yadav <yaduvanshishwetang@gmail.com>
2026-03-06 19:20:53 +0530

    creating authentication

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    connectDb()
+  console.log(`server started at ${port}`)
+  
+})
+
+
+
+
+
