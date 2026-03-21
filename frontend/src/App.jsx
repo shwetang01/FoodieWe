d8bcb754b1edbc1b02b17e9f66dcd895c753ab3f
Shwetang Yadav <yaduvanshishwetang@gmail.com>
2026-03-21 12:06:47 +0530

    password reset functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
+export const serverUrl="http://localhost:8000" 
 
 function App() {
   return (
     <Routes>
       <Route path='/signup' element={<SignUp/>} />
        <Route path='/signin' element={<SignIn/>} />
-
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
 
 
     </Routes>
