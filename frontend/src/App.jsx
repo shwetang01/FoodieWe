8ec8f893873e28a2da9261e5ebd577cd18111fd5
Shwetang Yadav <yaduvanshishwetang@gmail.com>
2026-06-09 17:49:37 +0530

    setting redux toolkit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
+import useGetCurrentUser from './hooks/useGetCurrentUser'
 export const serverUrl="http://localhost:8000" 
 
 function App() {
+  useGetCurrentUser()
+
   return (
     <Routes>
+       <Route path="/" element={<Navigate to="/signin" />} />
       <Route path='/signup' element={<SignUp/>} />
        <Route path='/signin' element={<SignIn/>} />
       <Route path='/forgot-password' element={<ForgotPassword/>} />
